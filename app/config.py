540242dfd9096950730c9c718282a6033afdd8ae
ferasalnaem <feras_alnaem@outlook.com>
2025-01-12 10:54:58 +0100

    Add Prod env

--- app/config.py
+++ app/config.py
@@ -1,5 +1,10 @@
 import os
 
 class Config:
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/newsAggregator")
+    # For local DB
+    # MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/newsAggregator")
+    # For Dockerized DB
+    #MONGO_URI = os.getenv("MONGO_URI", "mongodb://fzaher:dev@mongo:27017/newsAggregator?authSource=admin")
+    # For EC2 DB
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb://fzaher:dev@3.73.84.1:27017/newsAggregator?authSource=admin")
     MODEL_PATH = os.getenv("MODEL_PATH", "./models/news_classifier_bert")
